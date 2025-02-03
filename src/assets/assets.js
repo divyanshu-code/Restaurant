c6b856ddb4218d889fdae844a0085b67022fee58
divyanshu <divyanshubisht5734@gmail.com>
2025-02-04 00:52:33 +0530

    update

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -27,7 +27,7 @@ import sabji from './sabji.png'
 import addicon from './image.png'
 import rating from './image1.png'
 import remove from './image2.png'
-import photo from './Untitled design (2).png'
+import photo from './black.png'
 import app_store from './app_store.png'
 import play_store from './play_store.png'
 
